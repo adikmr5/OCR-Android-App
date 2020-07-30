5bce5697e2b22eeb833a2d41e0383097a332c6bd
Aditya Kumar <adikmr567@gmail.com>
2020-07-30 21:08:51 +0530

    added external lib to crop the image and detect Text

--- app/src/main/java/com/example/android/searchyourdoubts/MainActivity.java
+++ app/src/main/java/com/example/android/searchyourdoubts/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.searchyourdoubts;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 
 import androidx.annotation.NonNull;
@@ -22,7 +25,10 @@ import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     Bitmap imageBitmap2;
+    Uri uri,pass;
 
 
     @Override
@@ -46,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
         imageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"1",Toast.LENGTH_SHORT);
-                dispatchTakePictureIntent();
+                CropImage.startPickImageActivity(MainActivity.this);
+
 
             }
         });
@@ -64,24 +71,29 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageBitmap2=imageBitmap;
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imageBitmap2=imageBitmap;
+//            imageView.setImageBitmap(imageBitmap);
+//        }
+//    }
     private void showOutput() {
 
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap2);
+        FirebaseVisionImage image = null;
+        try {
+            image = FirebaseVisionImage.fromFilePath(this,pass);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FirebaseVisionTextRecognizer textRecognizer= FirebaseVision.getInstance().getOnDeviceTextRecognizer();
         textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
             @Override
@@ -111,4 +123,36 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Toast.makeText(this,""+requestCode+"  "+resultCode,Toast.LENGTH_SHORT).show();
+        if (requestCode == 200 && resultCode== Activity.RESULT_OK) {
+            Uri imageuri=CropImage.getPickImageResultUri(this,data);
+            imageView.setImageURI(imageuri);
+            if(CropImage.isReadExternalStoragePermissionsRequired(this,imageuri)) {
+                uri = imageuri;
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},0);
+
+            }
+            else{
+                startCrop(imageuri);
+            }
+        }
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+            if(resultCode==RESULT_OK){
+                imageView.setImageURI(result.getUri());
+                pass=result.getUri();
+
+            }
+        }
+    }
+
+    private void startCrop(Uri imageuri) {
+        CropImage.activity(imageuri)
+        .setGuidelines(CropImageView.Guidelines.ON)
+        .setMultiTouchEnabled(true)
+        .start(this);
+    }
+
 }
